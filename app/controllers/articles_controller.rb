d82078dce76c5fe8832ae6cdd6bf36dbac905f70
Rail Project <weyjones@gmail.com>
2015-12-21 18:36:54 -0800

    add creat and edit functionality

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ActionController::Base
+  def new
+  	@article = Article.new
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def create
+  	#render plain: params[:article].inspect
+  	@article = Article.new(article_params)
+  	if @article.save
+  		flash[:notice] = "created successfully"
+  		redirect_to article_path(@article)
+  	else
+  		render 'new'
+  	end
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	if @article.update(article_params)
+  		flash[:notice] = "update successfully"
+  		redirect_to article_path(@article)
+  	else
+  		render 'edit'
+  	end
+  end
+
+
+
+  def show
+	@article = Article.find(params[:id])
+  end
+
+  private
+  	def article_params
+  		params.require(:article).permit(:title, :description)
+  	end
+end
